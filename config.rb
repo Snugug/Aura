f12b22084dcc61e19be6783b8afeddc213ed4fc3
Sam Richard <srichard@workhabit.com>
2012-03-02 08:26:57 -0500

    Now requires Sassy Math.

--- config.rb
+++ config.rb
@@ -1,6 +1,7 @@
 # Require any additional compass plugins here.
 require 'susy'
 require 'modular-scale'
+require 'sassy-math'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
